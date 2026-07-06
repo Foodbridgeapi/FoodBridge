d984e6ead32c1b666a6343a04c6fb3cf3dcb4f2b
The Joel <jotelfootball@gmail.com>
2026-07-06 19:37:37 +0100

    feat: add comment 1 to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { DarkModeProvider } from './contexts/DarkModeContext'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import PostFood from './pages/PostFood'
+import Listings from './pages/Listings'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
+import Favorites from './pages/Favorites'
+import Admin from './pages/Admin'
+
+function App() {
+  return (
+    <DarkModeProvider>
+      <BrowserRouter>
+        <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/post" element={<PostFood />} />
+            <Route path="/listings" element={<Listings />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </DarkModeProvider>
+  )
+}
+
+export default App
+
+// Micro change commit 1
